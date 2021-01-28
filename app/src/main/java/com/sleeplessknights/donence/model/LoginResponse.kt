f0d9ec7d056022af9410b0b7d9a98f27289791f3
OnurDz <onrdemirezen@gmail.com>
2021-01-28 15:02:23 +0300

    style: types more clear

--- app/src/main/java/com/sleeplessknights/donence/model/LoginResponse.kt
+++ app/src/main/java/com/sleeplessknights/donence/model/LoginResponse.kt
@@ -2,8 +2,8 @@ package com.sleeplessknights.donence.model
 
 data class LoginResponse(
     val accessToken: String,
-    val email: String,
-    val username: String,
-    val role: String,
-    val name: String
+    val email:       String,
+    val username:    String,
+    val role:        String,
+    val name:        String
 )
