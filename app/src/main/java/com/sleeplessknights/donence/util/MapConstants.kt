369b15e6b50a911f19b69e7bd775d418308be426
Cihan <cihan.alma96@gmail.com>
2021-03-26 23:54:57 +0300

    fix: change map constants

--- app/src/main/java/com/sleeplessknights/donence/util/MapConstants.kt
+++ app/src/main/java/com/sleeplessknights/donence/util/MapConstants.kt
@@ -1,7 +1,7 @@
 package com.sleeplessknights.donence.util
 
 object MapConstants {
-    const val LOCATION_UPDATE_INTERVAL = 10000L
-    const val LOCATION_UPDATE_FASTEST_INTERVAL = 5000L
-    const val MAP_ZOOM = 15f
+    const val LOCATION_UPDATE_INTERVAL = 1000L
+    const val LOCATION_UPDATE_FASTEST_INTERVAL = 500L
+    const val MAP_ZOOM = 25f
 }
