f760964542fbd3c4fc984991f3bdece0e6a8fff1
Cihan <cihan.alma96@gmail.com>
2021-03-27 02:05:32 +0300

    refactor: indentation

--- app/src/main/java/com/sleeplessknights/donence/rest/address/AddressRepository.kt
+++ app/src/main/java/com/sleeplessknights/donence/rest/address/AddressRepository.kt
@@ -1,8 +1,8 @@
 package com.sleeplessknights.donence.rest.address
 
-import com.sleeplessknights.donence.ui.address.AddressLiveData
 import com.sleeplessknights.donence.data.AddressData
 import com.sleeplessknights.donence.data.model.AddressItem
+import com.sleeplessknights.donence.ui.address.AddressLiveData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import retrofit2.Call
@@ -13,15 +13,15 @@ class AddressRepository() {
         AddressApiService.create()
     }
 
-    suspend fun setAddress(headerToken:String, address: AddressLiveData): Call<AddressItem> {
+    suspend fun setAddress(headerToken: String, address: AddressLiveData): Call<AddressItem> {
 
         return withContext(Dispatchers.IO) {
             /* backend struggle starts here */
             val son = ("Bearer ".plus(headerToken))
             return@withContext adressApiService.submitAddress(son,
-            AddressData(address.value!!.latitude, address.value!!.longitude))
+                AddressData(address.value!!.latitude, address.value!!.longitude))
 
-             // !! don't forget TODO: get rid of !!s
+            // !! don't forget TODO: get rid of !!s
         }
     }
 }
