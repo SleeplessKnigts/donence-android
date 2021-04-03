61638caa9a2afb70e4172aa8b6e59f54f71fde23
Onur Demirezen <onrdemirezen@gmail.com>
2021-04-03 21:46:13 +0300

    fix: request data type field

--- app/src/main/java/com/sleeplessknights/donence/model/Request.kt
+++ app/src/main/java/com/sleeplessknights/donence/model/Request.kt
@@ -3,5 +3,5 @@ package com.sleeplessknights.donence.model
 import com.google.gson.annotations.SerializedName
 
 data class Request(
-    @SerializedName("request_type") val requestType: String
+    @SerializedName("requestType") val requestType: String
 )
