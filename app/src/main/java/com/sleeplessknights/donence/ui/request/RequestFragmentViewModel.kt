87c785cf68420011a05c3bcc93b04ec1b550712e
Onur Demirezen <onrdemirezen@gmail.com>
2021-04-14 19:48:30 +0300

    feat: launch recycle points from request fragment

--- app/src/main/java/com/sleeplessknights/donence/ui/request/RequestFragmentViewModel.kt
+++ app/src/main/java/com/sleeplessknights/donence/ui/request/RequestFragmentViewModel.kt
@@ -1,28 +1,42 @@
 package com.sleeplessknights.donence.ui.request
 
+import android.content.Intent
+import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.sleeplessknights.donence.MainActivity
 import com.sleeplessknights.donence.model.RequestResponse
 import com.sleeplessknights.donence.rest.request.RequestRepository
+import com.sleeplessknights.donence.ui.recyclepoints.RecyclePointsActivity
 import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Response
 import javax.security.auth.callback.Callback
+
 
 class RequestFragmentViewModel(
     private val requestRepository: RequestRepository
 ) : ViewModel() {
 
     private val isClicked = MutableLiveData<Boolean>()
+    private val launchRecyclePointsActivity = MutableLiveData<Boolean>()
 
     fun onClicked() {
         isClicked.value = true
     }
 
+    fun onShowRecyclePoints() {
+        launchRecyclePointsActivity.value = true
+    }
+
     fun getIsClicked(): LiveData<Boolean> {
         return isClicked
+    }
+
+    fun getLaunchRecyclePointsActivity(): LiveData<Boolean> {
+        return launchRecyclePointsActivity
     }
 
     fun makeRequest(token: String, requestType: String) {
