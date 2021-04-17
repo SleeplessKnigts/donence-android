60faa4d15ea93328d6e5624c76998cf2762f586a
Onur Demirezen <onrdemirezen@gmail.com>
2021-04-17 21:14:00 +0300

    fix: null pointer for recycle points

--- app/src/main/java/com/sleeplessknights/donence/model/RecyclePoint.kt
+++ app/src/main/java/com/sleeplessknights/donence/model/RecyclePoint.kt
@@ -6,7 +6,9 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class RecyclePoint(
+    @SerializedName("recyclePointId") val id: Int,
     @SerializedName("recyclePointDetail") val detail: String,
     @SerializedName("lat") val lat: Double,
-    @SerializedName("lng") val lng: Double
-) : Parcelable
+    @SerializedName("lng") val lng: Double,
+    @SerializedName("recyclePointPlaceType") val placeType: String
+    ) : Parcelable
--- app/src/main/java/com/sleeplessknights/donence/ui/recyclepoints/RecyclePointsActivity.kt
+++ app/src/main/java/com/sleeplessknights/donence/ui/recyclepoints/RecyclePointsActivity.kt
@@ -3,10 +3,12 @@ package com.sleeplessknights.donence.ui.recyclepoints
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
 import com.sleeplessknights.donence.R
 import com.sleeplessknights.donence.base.getAny
@@ -27,21 +29,30 @@ class RecyclePointsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        val pointList = getRecyclePoints()
+        val pointList = getRecyclePoints(googleMap)
+        updateMap(googleMap, pointList)
+    }
+
+    private fun getRecyclePoints(map: GoogleMap): ArrayList<RecyclePoint>? {
+        val token = getSharedPreferences(packageName, Context.MODE_PRIVATE)
+            .getAny<LoginResponse>("user_responseBody")!!.accessToken
+        val pointList: ArrayList<RecyclePoint> = ArrayList()
+        viewModel.getPointList(token).observe(this, Observer { result ->
+            pointList.addAll(result)
+            updateMap(map, pointList)
+        })
+        return pointList
+    }
+
+    private fun updateMap(map: GoogleMap, pointList: ArrayList<RecyclePoint>?) {
         for (point in pointList!!) {
-            googleMap.addMarker(
+            map.addMarker(
                 MarkerOptions()
                     .position(LatLng(point.lat, point.lng))
                     .title(point.detail)
             )
         }
-    }
 
-    private fun getRecyclePoints(): List<RecyclePoint>? {
-        val token = getSharedPreferences(packageName, Context.MODE_PRIVATE)
-            .getAny<LoginResponse>("user_responseBody")!!.accessToken
-        val pointList = viewModel.getPointList(token)
-        return pointList.value
     }
 
     private fun initViewModel(): RecyclePointsViewModel {
