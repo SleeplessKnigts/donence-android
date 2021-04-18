2b8df51951728df4ad62fd8ff2ddc709a750798c
Onur Demirezen <onrdemirezen@gmail.com>
2021-04-18 15:57:38 +0300

    request list text adjustments

--- app/src/main/java/com/sleeplessknights/donence/ui/requestlist/RequestListFragment.kt
+++ app/src/main/java/com/sleeplessknights/donence/ui/requestlist/RequestListFragment.kt
@@ -29,7 +29,6 @@ class RequestListFragment : Fragment() {
     ): View {
 
         binding = FragmentRequestlistBinding.inflate(inflater)
-
         return binding.root
     }
 
@@ -51,35 +50,67 @@ class RequestListFragment : Fragment() {
             var newItems: ArrayList<String> = ArrayList()
             newItems.add("Active requests:")
 
-            var status: String
             var date: String
             var type: String
             var item: String
 
+            var activeFlag: Int = 0
             for(r in result) {
                 if(r.active) {
-                    status = "Active"
-                    date = r.creationDate
-                    type = r.requestType
-                    item = status + " " + type + " " + date
+                    activeFlag++
+                    date = getFromattedDate(r.creationDate)
+                    type = getFormattedType(r.requestType)
+                    item = getFormattedItem(date, type)
                     newItems.add(item)
                 }
             }
+            if(activeFlag == 0) {
+                newItems.add("You do not have active requests.")
+            }
 
+            newItems.add("")
             newItems.add("Completed requests: ")
-
+            var oldFlag: Int = 0;
             for(r in result) {
                 if(!r.active) {
-                    status = "Completed"
-                    date = r.creationDate
-                    type = r.requestType
-                    item = status + " " + type + " " + date
+                    oldFlag++
+                    date = getFromattedDate(r.creationDate)
+                    type = getFormattedType(r.requestType)
+                    item = getFormattedItem(date, type)
                     newItems.add(item)
                 }
+            }
+            if(oldFlag == 0) {
+                newItems.add("You do not have completed requests.")
             }
 
             adapter.updateData(newItems)
         })
+    }
+
+    private fun getFormattedItem(date: String, type: String): String {
+        return "$type request created at $date"
+    }
+
+    private fun getFormattedType(requestType: String): String {
+
+        return when (requestType) {
+            "P" -> "Plastic"
+            "K" -> "Paper"
+            "G" -> "Glass"
+            "B" -> "Battery"
+            "E" -> "Electronic"
+            "O" -> "Oil"
+            else -> "Unknown"
+        }
+    }
+
+    private fun getFromattedDate(creationDate: String): String {
+        val day = creationDate.substring(8, 10)
+        val month = creationDate.substring(5, 7)
+        val year = creationDate.substring(0, 4)
+
+        return "$day.$month.$year"
     }
 
     private fun initViewModel(): RequestListFragmentViewModel {
